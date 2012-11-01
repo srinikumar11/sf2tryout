1c4cdd2bfc0808afdd3cd7bb8a410c2a5ab8f5e1
Srinivasan <srinivasan@comogroupap.com>
2012-11-01 14:37:18 +0530

    edit error fixed

--- app/autoload.php
+++ app/autoload.php
@@ -2,22 +2,18 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
 // intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+if (!function_exists('intl_get_error_code'))
+{
+    require_once __DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('Stfalcon',  __DIR__.'/../vendor/bundles');
-     $loader->add(array(
-    // ...
-    'Knp\Bundle' => __DIR__.'/../vendor/bundles',
-    'Knp\Menu'   => __DIR__.'/../vendor/KnpMenu/src',
-         'Doctrine\Common\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
-    
-    // ...
-));
+    $loader->add('', __DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+    $loader->add('Stfalcon', __DIR__ . '/../vendor/bundles');
+    $loader->add('Knp\Bundle', __DIR__ . '/../vendor/bundles');
+    $loader->add('Knp\Menu', __DIR__ . '/../vendor/KnpMenu/src');
+    $loader->add('Doctrine\Common\DataFixtures', __DIR__ . '/../vendor/doctrine-fixtures/lib');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
